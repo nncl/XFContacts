4288cf4f45cfe729bffbeda83cde247af68fc5c7
Cauê Almeida <nncl@live.com>
2017-05-17 18:57:17 -0300

    initial commit

--- XFContacts2/XFContacts2/MainPage.xaml.cs
+++ XFContacts2/XFContacts2/MainPage.xaml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using XFContacts2.Model;
 
 namespace XFContacts2
 {
@@ -12,6 +13,37 @@ namespace XFContacts2
         public MainPage()
         {
             InitializeComponent();
+
+            LoadContacts();
         }
+
+        private void LoadContacts()
+        {
+            var contacts = new List<Contato>();
+
+            Contato contact = new Contato();
+            contact.Name = "Caue Almeida";
+            contact.Phone = "+5511952053515";
+
+            Contato contact2 = new Contato();
+            contact2.Name = "Lucia Almeida";
+            contact2.Phone = "+5511973836244";
+
+            Contato contact3 = new Contato();
+            contact3.Name = "Sérgio Almeida";
+            contact3.Phone = "+5511998928514";
+
+            contacts.Add(contact);
+            contacts.Add(contact2);
+            contacts.Add(contact3);
+
+            lstContact.ItemsSource = contacts;
+        }
+
+        private void lstContact_ItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            DisplayAlert("Tapped", "Tapped", "Ok");
+        }
+
     }
 }
---
+++ XFContacts2/XFContacts2/Model/Contato.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XFContacts2.Model
+{
+    public class Contato
+    {
+        public String Name { get; set; }
+        public String Phone { get; set; }
+    }
+}
